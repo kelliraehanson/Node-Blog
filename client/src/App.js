bfbbc07e9c35d55d266854e22773ad956e60fb82
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-13 17:50:32 -0600

    react app working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const url = "http://localhost:7777/api/users"
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    users: [],
+
+  }
+}
+
+componentDidMount() {
+  axios.get(`${url}/`)
+  .then(res => {
+    this.setState({
+      users: res.data
+    })
+  })
+}
   render() {
     return (
       <div className="App">
+        <div className="users">
+        {this.state.users.map(user =>
+        <div className="user"> 
+        <h1>{user.name}</h1>
+        </div>
+        ) 
+        })}
         
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const usersRouter = require('./users/users-router.js');
 // const postsRouter = require("./posts/posts-router.js");
@@ -9,6 +10,7 @@ const server = express();
 
 // global middleware
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
 
